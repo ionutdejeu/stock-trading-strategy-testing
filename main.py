903aa42fb6586379cb653d5328ac8156c7de3ef0
ionut.dejeu <ionut.dejeu@gmail.com>
2022-05-03 09:20:28 +0300

    WIP Downloading indicator data

---
+++ main.py
@@ -0,0 +1,21 @@
+# Raw Package
+import numpy as np
+import pandas as pd
+from pandas_datareader import data as pdr
+
+# Market Data
+import yfinance as yf
+
+# Graphing / Visualization
+import plotly.graph_objs as go
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
